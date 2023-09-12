bef162028609668fe1a2c82ae22ccb7cd1b46efb
Amit Saha <asaha.bwn2000@gmail.com>
2023-09-13 00:14:01 +0530

    Services section completed

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,13 @@
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </div>
   )
 }
 
