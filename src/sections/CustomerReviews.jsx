1038edba192c61d4f7086a2d09aa629ec2f504fd
Amit Saha <asaha.bwn2000@gmail.com>
2023-09-14 00:49:10 +0530

    Footer section completed

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -7,7 +7,7 @@ const CustomerReviews = () => {
       <h3 className="text-center font-palanquin text-4xl font-bold">
         What Our <span className="text-coral-red">Customers </span>Say?
       </h3>
-      <p className="text-center info-text m-auto max-w-lg">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
+      <p className="text-center info-text m-auto mt-4 max-w-lg">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
         {reviews.map((review) => (
           <ReviewCard 
